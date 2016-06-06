869b876feb40118e6f0f8fe11944e98d576f3b9e
packman14 <packman14@gmail.com>
2016-06-06 11:53:00 +0200

    Partie creation

---
+++ src/main/java/streaming/service/PartieService.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import streaming.entity.Partie;
+
+/**
+ *
+ * @author ajc
+ */
+@Service
+public class PartieService 
+{
+
+    @Autowired
+    private PartieCRUDService pcs;
+    
+    public void joueurSuivant(Long partieID)
+    {
+        Partie partieActuelle = pcs.findOne(partieID);
+        int joueurActuel = partieActuelle.getNumeroSorciere();
+        if(joueurActuel < partieActuelle.getNbJoueurs())
+        {
+            partieActuelle.setNumeroSorciere(++joueurActuel);
+        }
+        else
+        {
+            partieActuelle.setNumeroSorciere(1);
+        }
+        pcs.save(partieActuelle);
+    }
+}
